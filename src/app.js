b965e243d16f9f319566b426985abe7ae251f101
Vertika <mittalvertika1803200@gmail.com>
2021-06-04 23:42:14 +0530

    News Template changes made

--- src/app.js
+++ src/app.js
@@ -9,44 +9,68 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 var cday = new Date();
 var ab = null;
-var cityName=null;
+var cityName = null;
 const monthNames = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
-  const dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-  var hours = cday.getHours();
-  var minutes = cday.getMinutes();
-  var ampm = hours >= 12 ? "pm" : "am";
-  hours = hours % 12;
-  hours = hours ? hours : 12; // the hour '0' should be '12'
-  var strTime = hours + ":" + minutes + " " + ampm;
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+const dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+var hours = cday.getHours();
+var minutes = cday.getMinutes();
+var ampm = hours >= 12 ? "pm" : "am";
+hours = hours % 12;
+hours = hours ? hours : 12; // the hour '0' should be '12'
+var strTime = hours + ":" + minutes + " " + ampm;
 
+//for news page
+var newsposts = [];
 
 app.get("/", (req, res) => {
   res.render("index");
 });
-app.post("/",(req,res)=>{
-  cityName=req.body.city;
+app.post("/", (req, res) => {
+  cityName = req.body.city;
   res.redirect("/get_temp");
-})
+});
+app.get("/get_temp", (req, res) => {
+  async function axiosTest() {
+    ab = await axios.get(
+      "http://api.openweathermap.org/data/2.5/weather?appid=cc6ce66bf521f3f06410403123840307&units=metric",
+      { params: { q: cityName } }
+    );
+    return ab;
+  }
+  axiosTest()
+    .then((data) => {
+      res.render("temp", {
+        location: cityName,
+        day: dayNames[cday.getDay()],
+        month: monthNames[cday.getMonth()],
+        date: cday.getDate(),
+        time: strTime,
+        currenttemp: data.data.main.temp,
+        mintemp: data.data.main.temp_min,
+        maxtemp: data.data.main.temp_max,
+      });
+    })
+    .catch((err) => console.log(err));
+});
+
 // app.get("/get_temp", (req, res) => {
 
-
 //   async function axiosTest() {
-//     ab = await axios.get(
-//       "http://api.openweathermap.org/data/2.5/weather?appid=cc6ce66bf521f3f06410403123840307"
-//     ,{ params: { q: cityName} });
+//     ab = await axios.get("https://api.weatherbit.io/v2.0/current?city=Meerut&key=83ffd4ff1807490e8dde456a3a909283");
+//     console.log(ab);
 //     return ab;
 
 //   }
@@ -58,37 +82,33 @@ app.post("/",(req,res)=>{
 //         month: monthNames[cday.getMonth()],
 //         date: cday.getDate(),
 //         time: strTime,
-//         currenttemp: data.data.main.temp,
-//         mintemp:data.data.main.temp_min,
-//         maxtemp:data.data.main.temp_max,
-//       });  
+//         currenttemp: ab.data.data[0].temp,
+//         mintemp:20,
+//         maxtemp:30,
+//       });
 //   })
-//   // .catch(err => console.log(err))
+//   .catch(err => console.log(err))
 // });
 
-app.get("/get_temp", (req, res) => {
+app.get("/get_news", (req, res) => {
 
 
   async function axiosTest() {
     ab = await axios.get(
-      "https://api.weatherbit.io/v2.0/current?city=Meerut&key=83ffd4ff1807490e8dde456a3a909283");
+      "https://newsapi.org/v2/everything?language=en&sortBy=popularity&apiKey=8576618518634db4bd1044cd43dc64ba",
+      { params: { q:cityName,qInTitle:cityName,sources:"bbc-news",domains:"bbc.co.uk" } }
+    );
     return ab;
-
   }
   axiosTest()
-  .then(data => {
-    res.render("temp", {
-        location: cityName,
-        day: dayNames[cday.getDay()],
-        month: monthNames[cday.getMonth()],
-        date: cday.getDate(),
-        time: strTime,
-        currenttemp: data.data.temp,
-        mintemp:20,
-        maxtemp:30,
-      });  
-  })
-  .catch(err => console.log(err))
+    .then((data) => {
+      // console.log();
+      res.render("news", {
+        headings: ab.data.articles[0].title,
+        newsinshort:ab.data.articles[0].content
+          });
+    })
+    .catch((err) => console.log(err));
 });
 
 app.listen(port, () => {
